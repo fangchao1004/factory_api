4b771cf1155ad135e9379f6201fbcaf5d12b7d93
fc <1018555611@qq.com>
2019-06-05 16:10:05 +0800

    后台修改。支持上传各种类型文件。

--- bug.js
+++ bug.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize')
 const fs = require('fs')
+const path = require('path')
+const uuidv1 = require('uuid/v1')
+const send = require('koa-send')
+const url = require("url")
+const querystring = require("querystring")
 
 module.exports = function (router, sequelize, logger) {
 
@@ -13,13 +18,13 @@ module.exports = function (router, sequelize, logger) {
                 primaryKey: true,
                 autoIncrement: true
             },
-            record_id: Sequelize.INTEGER(11),
             device_id: Sequelize.INTEGER(11),
             user_id: Sequelize.INTEGER(11),
             fixed_user_id: Sequelize.INTEGER(11),
             status: Sequelize.INTEGER(1),
             content: Sequelize.STRING(100),
             buglevel: Sequelize.INTEGER(1),
+            area_remark: Sequelize.STRING(100),
         },
         {
             timestamps: true
@@ -28,10 +33,6 @@ module.exports = function (router, sequelize, logger) {
 
     router.post('/insert_bug', async (ctx, next) => {
         try {
-            let copyRequsetBody = JSON.parse(JSON.stringify(ctx.request.body));
-            let content = JSON.parse(copyRequsetBody.content);
-            let contentAfterTrans = await transformHandler(content);
-            ctx.request.body.content = JSON.stringify(contentAfterTrans);
             let result = await Bugs.create(ctx.request.body)
             ctx.response.type = 'json'
             ctx.response.body = { code: 0, data: result }
@@ -81,42 +82,27 @@ module.exports = function (router, sequelize, logger) {
         }
     })
 
-    async function transformHandler(content) {
-        // console.log("初始数据：" +  content.imgs.length);
-        let newPathValueArr = await writeImages(content.imgs);
-        // console.log("newPathValueArr:::", newPathValueArr);
-        content.imgs = newPathValueArr;////["路径1","路径2",....]
-        return content;
-    }
-    async function writeImages(imageArr) {
-        let pathArr = [];
-        for (const item of imageArr) {
-            try { ////捕获Promise的异常
-                let OneImgPath = await writeOneImg(item);
-                pathArr.push(OneImgPath);
-            } catch (error) {
-                console.log('error:' + error);
-                return [];
-            }
-        }
-        // logger.debug('全部写入成功', pathArr);
-        return pathArr;
-    }
-    function writeOneImg(item) {
-        ////最底层的异步操作，要包含在Promise方法中。用于直接调用 resolve 或 reject
-        let p = new Promise(function (resolve, reject) {        //做一些异步操作
-            let oneImgPath = '/Users/fangchao/Desktop/imageKU/' + new Date().getTime() + '.jpg';
-            var base64Data = item.replace(/^data:image\/\w+;base64,/, "");
-            let dataBuffer = new Buffer(base64Data, 'base64'); //把base64码转成buffer对象，
-            fs.writeFile(oneImgPath, dataBuffer, function (err) {//用fs写入文件
-                if (err) {
-                    reject('写入图片失败')
-                }
-                else {
-                    resolve(oneImgPath)
-                }
-            })
-        });
-        return p;
-    }
+
+    router.post('/upload_file', async (ctx, next) => {
+        // 上传单个文件
+        const file = ctx.request.files.file; // 获取上传文件
+        const reader = fs.createReadStream(file.path)
+        const uuid = uuidv1()
+        let filePath = path.join(__dirname, 'public/upload/') + `/${uuid}.${file.name.split('.')[file.name.split('.').length - 1]}`;
+        // // 创建可写流
+        const upStream = fs.createWriteStream(filePath);
+        // // 可读流通过管道写入可写流
+        reader.pipe(upStream);
+        ctx.response.type = 'json'
+        ctx.response.body = { code: 0, data: uuid }
+    })
+
+    router.get('/get_jpg', async (ctx) => {
+        const arg = url.parse(ctx.req.url).query;
+        const params = querystring.parse(arg);
+        const uuid = params.uuid
+        let filePath = path.join('public/upload/') + `/${uuid}.jpg`;
+        ctx.attachment(filePath)
+        await send(ctx, filePath)
+    })
 }
--- record.js
+++ record.js
@@ -1,11 +1,4 @@
 const Sequelize = require('sequelize')
-const fs = require('fs')
-const path = require('path')
-const uuidv1 = require('uuid/v1')
-const send = require('koa-send')
-const url = require("url")
-const querystring = require("querystring")
-
 
 module.exports = function (router, sequelize, logger) {
 
@@ -30,16 +23,8 @@ module.exports = function (router, sequelize, logger) {
       timestamps: true
     }
   )
-  ///////////////////
   router.post('/insert_record', async (ctx, next) => {
     try {
-      let copyRequsetBody = JSON.parse(JSON.stringify(ctx.request.body));
-      let content = JSON.parse(copyRequsetBody.content);
-      // logger.debug('初始化数据：',content);
-      let contentAfterTrans = await transformHandler(content);
-      ctx.request.body.content = JSON.stringify(contentAfterTrans);
-      // logger.debug('我最后的返回值结果：', ctx.request.body);
-      // return;
       await Records.create(ctx.request.body)
       ctx.response.type = 'json'
       ctx.response.body = { code: 0, data: 'success' }
@@ -49,49 +34,6 @@ module.exports = function (router, sequelize, logger) {
       ctx.response.body = { code: -1, data: 'fault' }
     }
   })
-
-  async function transformHandler(content) {
-    // logger.debug("初始数据：" + JSON.stringify(content));
-    for (let element of content) {
-      if (element.type_id === "6" && element.value.length > 0) {  ///图片base64数组
-        // logger.debug('图片数据：',element.value, typeof element.value);///array
-        let newPathValueArr = await writeImages(element.value);
-        // logger.debug("处理后的数组：",newPathValueArr);
-        element.value = newPathValueArr;////["路径1","路径2",....]
-      }
-    };
-    return content;
-  }
-  async function writeImages(imageArr) {
-    let pathArr = [];
-    for (const item of imageArr) {
-      try { ////捕获Promise的异常
-        let OneImgPath = await writeOneImg(item);
-        pathArr.push(OneImgPath);
-      } catch (error) {
-        logger.debug('error:' + error);
-        return [];
-      }
-    }
-    // logger.debug('全部写入成功', pathArr);
-    return pathArr;
-  }
-  function writeOneImg(item) {
-    ////最底层的异步操作，要包含在Promise方法中。用于直接调用 resolve 或 reject
-    let p = new Promise(function (resolve, reject) {        //做一些异步操作
-      let oneImgPath = '/Users/fangchao/Desktop/imageKU/' + new Date().getTime() + '.jpg';
-      var base64Data = item.replace(/^data:image\/\w+;base64,/, "");
-      let dataBuffer = new Buffer(base64Data, 'base64'); //把base64码转成buffer对象，
-      fs.writeFile(oneImgPath, dataBuffer, function (err) {//用fs写入文件
-        if (err) { reject('写入图片失败') }
-        else {
-          resolve(oneImgPath)
-        }
-      })
-    });
-    return p;
-  }
-  ///////////////////////
   router.post('/find_record', async (ctx, next) => {
     try {
       let all = await Records.findAll({
@@ -131,30 +73,4 @@ module.exports = function (router, sequelize, logger) {
       ctx.response.body = { code: -1, data: 'update fault' }
     }
   })
-
-  router.post('/upload_file', async (ctx, next) => {
-    // 上传单个文件
-    console.log('upload file')
-    const file = ctx.request.files.audio; // 获取上传文件
-    console.log(ctx.request)
-    const reader = fs.createReadStream(file.path)
-    const uuid = uuidv1()
-    let filePath = path.join(__dirname, 'public/upload/') + `/${uuid}.png`;
-    // // 创建可写流
-    const upStream = fs.createWriteStream(filePath);
-    // // 可读流通过管道写入可写流
-    reader.pipe(upStream);
-    ctx.response.type = 'json'
-    ctx.response.body = { code: 0, data: uuid }
-  })
-
-  router.get('/get_audio', async (ctx) => {
-    const arg = url.parse(ctx.req.url).query;
-    const params = querystring.parse(arg);
-    const uuid = params.uuid
-    let filePath = path.join('public/upload/') + `/${uuid}.png`;
-    ctx.attachment(filePath)
-    await send(ctx, filePath)
-  })
-
 }
