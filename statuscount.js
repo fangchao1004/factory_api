cdc7285a6cd04df71ca18869a9ab1b402d207644
fc <1018555611@qq.com>
2019-07-09 12:40:16 +0800

    添加 statuscounts 表的  detect_num 字段。用于统计当天巡检了多少台设备

--- statuscount.js
+++ statuscount.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const schedule = require('node-schedule');
+const moment = require('moment');
 
 module.exports = function (router, sequelize, logger) {
 
@@ -15,7 +16,8 @@ module.exports = function (router, sequelize, logger) {
             },
             total_num: Sequelize.INTEGER(11),
             error_num: Sequelize.INTEGER(11),
-            normal_num: Sequelize.INTEGER(11)
+            normal_num: Sequelize.INTEGER(11),
+            detect_num: Sequelize.INTEGER(11),
         },
         {
             timestamps: true
@@ -23,7 +25,7 @@ module.exports = function (router, sequelize, logger) {
     )
     function setScheduleJob() {
         schedule.scheduleJob('55 59 23 * * *', () => {
-            logger.debug('每天的23:59:55点触发，去更新statuscount表');
+            logger.debug('每天的23:59:55点触发，去更新statuscount表'); ///一定要在当天的末尾
             doAction();
         });
     }
@@ -34,11 +36,18 @@ module.exports = function (router, sequelize, logger) {
         result[0].forEach((item) => {
             allCount += item.status_count
         })
+        let startOfToday = moment().startOf('day').format('YYYY-MM-DD HH:mm:ss');
+        let endOfToday = moment().endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        let sql1 = ' select count(*) as todayDetectCount  from (select distinct rds.device_id from records rds'
+        let sql2 = ' where rds.createdAt > "' + startOfToday + '" and rds.createdAt < "' + endOfToday + '") t1 ';
+        let sqlText2 = sql1 + sql2;
+        let result2 = await sequelize.query(sqlText2);
         ///将当前结果插入到statuscount表中
         let dataObj = {
             total_num: allCount,
             normal_num: result[0][0].status_count,
             error_num: result[0][1].status_count,
+            detect_num: result2[0][0].todayDetectCount,
         };
         // console.log(dataObj);//在一天结束的时刻 将当前的设备状态记录保存进表
         await Status_Count.create(dataObj)
