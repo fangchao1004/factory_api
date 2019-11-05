86d3781db4925b68fb765df5e70ace506b5cdeab
coco <wosxieez@qq.com>
2019-11-05 10:48:06 +0800

    切换数据库地址

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
   database: 'factory', // 使用哪个数据库
   username: 'root', // 用户名
   password: 'Xiebao0301@', // 口令
-  host: 'hefeixiaomu.com', // 主机名
+  host: 'ixiaomu.cn', // 主机名
   port: 3306 // 端口号，MySQL默认3306
 };
 
