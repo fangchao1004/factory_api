1d367ead260fc9c5c3e144db7957bd61e462c612
fc <1018555611@qq.com>
2019-11-28 09:52:41 +0800

    app 0.1.1 版本更新

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ var sequelize = new Sequelize(
 
 router.get('/version_update', async (ctx, next) => {
   ctx.response.type = 'json'
-  ctx.response.body = { code: 0, vn: '0.1.0' }
+  ctx.response.body = { code: 0, vn: '0.1.1' }
 })
 
 areaInit(router, sequelize, logger)
