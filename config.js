3eabf27c454b77967e36f345e78870587f2242b5
August <1018555611@qq.com>
2021-06-26 22:09:26 +0800

    补充提交

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
   database: 'factory', // 使用正式数据库
   username: 'root', // 用户名
-  password: 'Xiebao0301@', // 口令
-  host: 'ixiaomu.cn', // 主机名
+  password: 'Xiaomu2019@.', // 口令
+  host: '60.174.196.158', // 主机名
   port: 3306 // 端口号，MySQL默认3306
 };
 
